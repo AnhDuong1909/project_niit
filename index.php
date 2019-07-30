95b01bdb59b315623180e74147f6d917f3a28d72
= <=>
2019-07-30 14:35:29 +0700

    commit to respository

---
+++ index.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link type="text/css" rel="stylesheet" href="css/header.css">
+    <link type="text/css" rel="stylesheet" href="css/home_slides.css">
+    <link type="text/css" rel="stylesheet" href="css/san_pham.css">
+    <link type="text/css" rel="stylesheet" href="css/chitiet_sp.css">
+    <link type="text/css" rel="stylesheet" href="css/aside.css">
+    <link type="text/css" rel="stylesheet" href="css/footer.css">
+    <link type="text/css" rel="stylesheet" href="css/tin_tuc.css">
+</head>
+<body>
+    <section id="main" >
+        <!-- BEGIN: header -->
+        <header>
+            <div class="top_header">
+                <div class="logo">
+                    <a href="">
+                        <img src="img/logo.jpg">
+                    </a>
+                </div>
+                <div class="search">
+                    <form method="get" action="" name="search">
+                        <input type="text" name="search" placeholder="Bạn cần tìm sản phẩm nào?" >
+                        <input type="submit" value="Search">
+                    </form>
+                </div>
+            </div>
+            <nav>
+                <ul>
+                    <li class="home_nav">
+                        <a href=""><img src="img/home_p.png"></a>
+                    </li>
+                    <li>
+                        <a href=""><h3>ĐIỆN THOẠI</h3></a>
+                        <div class="home_nav_col">
+                            <div class="home_nav_col_sub">
+                                <ul>
+                                <!-- BEGIN: ds1_nsx -->
+                                    <li><a href="">Sam Sung</a></li>
+                                <!-- END: ds1_nsx -->
+                                </ul>
+                            </div>
+                            <div class="home_nav_col_sub" style="border:none;">
+                                <ul>
+                                <!-- BEGIN: ds2_nsx -->
+                                    <li><a href="">Nokía</a></li>
+                                <!-- END: ds2_nsx -->               
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href=""><h3>MÁY TÍNH BẢNG</h3></a>
+                        <div class="home_nav_col">
+                            <div class="home_nav_col_sub">
+                                <ul>
+                                <!-- BEGIN: ds3_nsx -->
+                                    <li><a href="">Sam sung</a></li>
+                                <!-- END: ds3_nsx -->
+                                </ul>
+                            </div>
+                            <div class="home_nav_col_sub" style="border:none;">
+                                <ul>
+                                <!-- BEGIN: ds4_nsx -->
+                                    <li><a href="">Apple</a></li>
+                                <!-- END: ds4_nsx -->
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href=""><h3>PHỤ KIỆN</h3></a>
+                        <div class="home_nav_col">
+                            <div class="home_nav_col_sub">
+                                <ul>
+                                <!-- BEGIN: ds1_dm_pk -->
+                                    <li><a href="">Tên dan mục phụ kiện</a></li>
+                                <!-- END: ds1_dm_pk -->
+                                </ul>
+                            </div>
+                            <div class="home_nav_col_sub" style="border:none;">
+                                <ul>
+                                <!-- BEGIN: ds2_dm_pk -->
+                                    <li><a href="">Tên danh mục phụ kiện</a></li>
+                                <!-- END: ds2_dm_pk -->               
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <a href=""><h3>SẢN PHẨM KHÁC</h3></a>
+                        <div class="home_nav_col2">
+                            <div class="home_nav_col_sub2">
+                                <ul>
+                                <!-- BEGIN: dm_spk -->
+                                    <li><a href="">Tên dm spk</a></li>
+                                <!-- END: dm_spk -->
+                                </ul>                                    
+                            </div> 
+                        </div>
+                    </li>
+                    <li>
+                        <a href=""><h3>MÁY DÙNG RỒI</h3></a>
+                    </li>
+                    <li>
+                        <a href=""><h3>KHUYẾN MÃI</h3></a>
+                    </li>
+                    <li>
+                        <a href=""><h3>TIN TỨC</h3></a>
+                    </li>
+                    <li>
+                        <a href=""><h3>ĐĂNG NHẬP</h3></a>
+                    </li>
+                    <li>
+                        <a href=""><h3>ĐĂNG KÍ</h3></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <!-- END: header -->
+                
+        <!-- BEGIN: home_slides -->
+        <section class="home_slides">
+             <section class="home_slides_img">
+                    <img src="img/homeslides/hinh1.jpg" style="height: 360px;">
+                </section> 
+                <section class="home_slides_news">
+                    <div class="home_slides_news_top">
+                        <a href=""><h1>TIN CÔNG NGHỆ</h1></a>
+                    </div>
+                    
+                        <!-- BEGIN: ds_tt -->
+                        <div class="home_slides_news_row">
+                            <div class="home_slides_news_row_left">
+                                <a href="">Tiêu đề bài viết </a>
+                                <div>Ngày đăng</div>
+                            </div>
+                            <div class="home_slides_news_row_right">
+                                 <img src="img/homeslides/hinh1.jpg"> 
+                            </div>
+                        </div><br>
+                        <!-- END: ds_tt -->
+                   
+                </section>
+            </section>
+        <!-- END: home_slides -->
+                        
+        <section class="content">
+         <!-- BEGIN: content_main -->
+             <section class="content_main" style="{tintuc}">
+                 <!-- BEGIN: san_pham -->
+    <div class="content_main_block">
+            <!-- BEGIN: block -->
+                <h4><a href="">Danh sách các loại máy </a></h4>
+                <div class="content_main_block_list">
+                <!-- BEGIN: list_sp -->
+                    <div class="content_main_block_list_sp">
+                        <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                        <h4>Tên sản phẩm</h4>
+                        <h6>giá sản phẩm</h6>
+                    </div>
+                    <div class="content_main_block_list_sp">
+                        <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                        <h4>Tên sản phẩm</h4>
+                        <h6>giá sản phẩm</h6>
+                    </div>
+                    <div class="content_main_block_list_sp">
+                        <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                        <h4>Tên sản phẩm</h4>
+                        <h6>giá sản phẩm</h6>
+                    </div>
+                    <div class="content_main_block_list_sp">
+                        <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                        <h4>Tên sản phẩm</h4>
+                        <h6>giá sản phẩm</h6>
+                    </div>
+                    <div class="content_main_block_list_sp">
+                        <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                        <h4>Tên sản phẩm</h4>
+                        <h6>giá sản phẩm</h6>
+                    </div>
+                    <div class="content_main_block_list_sp">
+                        <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                        <h4>Tên sản phẩm</h4>
+                        <h6>giá sản phẩm</h6>
+                    </div>
+                    <div class="content_main_block_list_sp">
+                        <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                        <h4>Tên sản phẩm</h4>
+                        <h6>giá sản phẩm</h6>
+                    </div>
+                  <!-- END: list_sp -->
+                    <div class="content_main_block_list_next">
+                        <h4><a href="">{end}</a></h4>
+                    </div>
+                </div>
+                
+        <!-- END: san_pham -->
+        <!-- BEGIN: pk -->
+            <div class="content_main_block">
+                    <!-- BEGIN: block -->
+                        <h4><a href="">Danh sách phụ kiện</a></h4>
+                        <div class="content_main_block_list">
+                        <!-- BEGIN: list_pk -->
+                            <div class="content_main_block_list_sp">
+                                <a href=""><img src="img/homeslides/hinh3.jpg"></a>
+                                <h4>Tên phụ kiện</h4>
+                                <h6>Giá phụ kiện</h6>
+                            </div>
+                            <div class="content_main_block_list_sp">
+                                <a href=""><img src="img/homeslides/hinh3.jpg"></a>
+                                <h4>Tên phụ kiện</h4>
+                                <h6>Giá phụ kiện</h6>
+                            </div>
+                            <div class="content_main_block_list_sp">
+                                <a href=""><img src="img/homeslides/hinh3.jpg"></a>
+                                <h4>Tên phụ kiện</h4>
+                                <h6>Giá phụ kiện</h6>
+                            </div>
+                            <div class="content_main_block_list_sp">
+                                <a href=""><img src="img/homeslides/hinh3.jpg"></a>
+                                <h4>Tên phụ kiện</h4>
+                                <h6>Giá phụ kiện</h6>
+                            </div>
+                        <!-- END: list_pk -->
+                            <div class="content_main_block_list_next">
+                                <h4><a href="">{end}</a></h4>
+                            </div>
+                        </div>
+                       
+                    <!-- END: block -->
+                    </div>
+        <!-- END: pk -->
+        <!-- BEGIN: spk -->
+        <div class="content_main_block">
+                <!-- BEGIN: block -->
+                    <h4><a href="">Danh sách sản phẩm khác</a></h4>
+                    <div class="content_main_block_list">
+                    <!-- BEGIN: list_spk -->
+                        <div class="content_main_block_list_sp">
+                            <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                            <h4>Ten san pham khác</h4>
+                            <h6>giá sản phẩm khác</h6>
+                        </div>
+                    <!-- END: list_spk -->
+                        <div class="content_main_block_list_next">
+                            <h4><a href="">{end}</a></h4>
+                        </div>
+                    </div>
+                    
+                <!-- END: block -->
+                </div>
+        <!-- END: spk -->
+            </section>
+         <!-- END: content_main -->
+        <!-- BEGIN: aside -->
+                 
+        <aside>
+                <div class="aside_block">
+                    
+                    <a href=""><h4>ĐANG HOT</h4></a>
+                    
+                   
+                    <ul>
+                        <li>
+                        <!-- BEGIN: dang_hot1 -->
+                            <div class="aside_block_sp">
+                                <a href=""><img src="img/homeslides/hinh3.jpg"></a>
+                                <h5>Tên sản phẩm</h5>
+                            </div>
+                        <!-- END: dang_hot1 -->
+                        </li>
+                        <li>
+                            <!-- BEGIN: dang_hot2 -->
+                            <div class="aside_block_sp">
+                                <a href=""><img src="img/homeslides/hinh2.jpg"></a>
+                                <h5>Tên sản phẩm</h5>
+                            </div>
+                        <!-- END: dang_hot2 -->
+                        </li>
+                    </ul>
+                </div><br>
+                <div class="aside_block">
+                    
+                    <a href=""><h4>HÀNG MỚI VỀ</h4></a>
+                        
+                   
+                    <ul>
+                        <li>
+                        <!-- BEGIN: hang_moi_ve1 -->
+                            <div class="aside_block_sp">
+                                <a href=""><img src="img/homeslides/hinh1.jpg"></a>
+                                <h5>Tên sản phẩm</h5>
+                            </div>
+                        <!-- END: hang_moi_ve1 -->
+                        </li>
+                        <li>
+                            <!-- BEGIN: hang_moi_ve2 -->
+                            <div class="aside_block_sp">
+                                <a href=""><img src="img/homeslides/hinh3.jpg"></a>
+                                <h5>Tên sản phẩm</h5>
+                            </div>
+                        <!-- END: hang_moi_ve2 -->
+                        </li>
+                    </ul>
+                </div><br>
+                <div class="aside_block">
+                
+                <a href=""><h4>SẢN PHẨM KHUYỄN MÃI</h4></a>
+                        
+                   
+                    <ul>
+                        <li>
+                        <!-- BEGIN: khuyen_mai1 -->
+                            <div class="aside_block_sp">
+                                <a href="#"><img src="img/homeslides/hinh1.jpg"></a>
+                                <h5>Tên sản phẩm</h5>
+                            </div>
+                        <!-- END: khuyen_mai1 -->
+                        </li>
+                        <li>
+                            <!-- BEGIN: khuyen_mai2 -->
+                            <div class="aside_block_sp">
+                                <a href="#"><img src="img/homeslides/hinh3.jpg"></a>
+                                <h5>Tên sản phẩm</h5>
+                            </div>
+                        <!-- END: khuyen_mai2 -->
+                        </li>
+                    </ul>
+                </div><br>
+                <div class="aside_block">
+                    <a href="#"><h4>DỊCH VỤ</h4></a>
+                    <p><strong><em>1.</em></strong><strong>Dịch vụ 1: </strong>
+                         abcdèghiklm</p>
+                    
+                </div>
+            </aside>
+    <!-- END: aside -->
+    
+                        </section>
+                    </section>
+         <!-- BEGIN: footer -->
+         
+        <footer>
+                <div class="div1">
+                    <a href="index.php"><img src="img/homeslides/hinh2.jpg" style="height: 150px"></a>
+                    <div class="div2">
+                        Địa chỉ
+                    </div>
+                    <div class="div3">
+                        <h3>HỆ THỐNG CỬA HÀNG</h3>
+                        <ul>
+                            <li>Quận Thanh XUân - ĐT:0123456789 </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+                <div class="div4">
+                <h3 style="font-size:12px;">Nhóm thực hiện: Trung tâm đào tạo công nghệ thông tin và truyền thông</h3>
+                </div>
+            </footer>
+    <!-- END: footer -->          
+     
+</body>
+</html>
